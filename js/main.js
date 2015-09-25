14087e843c781fa39a664814dde4c8948dd94217
MrLYC <imyikong@gmail.com>
2015-09-25 05:39:42 +0000

    update

--- js/main.js
+++ js/main.js
@@ -30,6 +30,15 @@
     
     function render_markdown(text) {
         $article_area.html(md_converter.makeHtml(text));
+        var $pre_area = $article_area.find("pre");
+        $pre_area.addClass("hljs");
+        $pre_area.find("code").each(function(i, block) {
+            hljs.highlightBlock(block);
+        });
+        $article_area.find("h1, h2, h3, h4, h5").each(function() {
+            var self = $(this);
+            self.attr("id", escape(self.text()));
+        });
     }
     
     function list_dir(dir_name, callback) {
@@ -76,6 +85,7 @@
         fetch_doc(item);
         $(".active-toc-item").removeClass("active-toc-item");
         $("#toc_item_" + item.sha).addClass("active-toc-item");
+        $("#content").scrollTop(0);
     }
     
     $(function init(){
@@ -86,21 +96,31 @@
         $toc_list = $("#toc-list");
         $article_area = $("#article");
         $title = $("title");
+        
+        hljs.initHighlightingOnLoad();
     
         render_markdown($("#md-default").text());
         list_dir(META.directory, function(data) {
+            var selected = document.location.hash.substr(1);
             var active_toc_item = null;
             for(var i in data){
                 var item = data[i];
                 if(item.name == META.default){
+                    data.splice(i, 1);
+                    data.splice(0, 0, item);
+                    if(selected.length == 0){
+                        active_toc_item = item;
+                        break;
+                    }
+                }
+                if(selected.length > 1 && item.name == selected){
                     active_toc_item = item;
-                    break;
                 }
             }
             
             render_toc(data);
             if(active_toc_item != null){
-                $(".toc-lnk[data-sha=" + item.sha + "]").click();
+                $(".toc-lnk[data-sha=" + active_toc_item.sha + "]").click();
             }
         });
     });
