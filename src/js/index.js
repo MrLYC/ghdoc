6f596b6cd49f8a0df96d5513d298f8db135010e9
MrLYC <imyikong@gmail.com>
2018-02-23 17:39:23 +0800

    add global config

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,22 @@
 import main from "./view"
 
-main({
-    base: "http://api.github.com",
-    user: "MrLYC",
-    repo: "ghdoc-test",
-    path: "",
+var config = Object.assign({
+    user: "",
+    repo: "",
+    path: "docs",
+    base: "//api.github.com",
     cacheSize: 10,
     fileListEl: "#file-list",
     fileContentEl: "#file-content",
     headerEl: "#header",
-});
+}, CONFIG);
+
+if (config.user == "") {
+    config.user = document.location.hostname.split(".", 1)[0];
+}
+
+if (config.repo == "") {
+    config.repo = document.location.pathname.split("/", 2)[1];
+}
+
+main(config);
