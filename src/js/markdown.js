4081577bbb2d9ae1ff4d0c51a99bb6190325eb95
MrLYC <imyikong@gmail.com>
2018-02-23 16:46:49 +0800

    add code highlight

--- src/js/markdown.js
+++ src/js/markdown.js
@@ -1,4 +1,12 @@
 import marked from "marked"
+import highlight from "highlight.js"
+
+highlight.initHighlightingOnLoad();
+marked.setOptions({
+    highlight: function (code) {
+        return highlight.highlightAuto(code).value;
+    },
+});
 
 export default function renderMarkdown(content) {
     return marked(content);
