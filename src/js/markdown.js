95950b5531f2fec87b35dbbf5c0f102c89c49f1a
MrLYC <imyikong@gmail.com>
2018-02-28 18:03:56 +0000

    update markdown render

--- src/js/markdown.js
+++ src/js/markdown.js
@@ -2,7 +2,16 @@ import marked from "marked"
 import highlight from "highlight.js"
 
 highlight.initHighlightingOnLoad();
+
+function renderer() {
+    var renderer = new marked.Renderer();
+    return renderer;
+}
+
 marked.setOptions({
+    gfm: true,
+    breaks: true,
+    renderer: renderer(),
     highlight: function (code, lang) {
         if (highlight.getLanguage(lang)) {
             return highlight.highlight(lang, code, true).value;
