35bcdcd0122dfdf88dab70f94b9a2b3725139567
MrLYC <imyikong@gmail.com>
2018-03-01 16:42:11 +0000

    add foornote

--- src/js/markdown.js
+++ src/js/markdown.js
@@ -1,14 +1,21 @@
 import MarkdownIt from 'markdown-it';
 import mkHighlight from 'markdown-it-highlightjs';
 import mkToc from 'markdown-it-github-toc';
+import mkFootnote from 'markdown-it-footnote';
 
-export default function renderMarkdown(content) {
+function getRender() {
     var render = new MarkdownIt({
         html: true,
     });
     render.use(mkHighlight);
+    render.use(mkFootnote);
     render.use(mkToc, {
         anchorClassName: "toc-anchor",
     });
+    return render;
+}
+
+export default function renderMarkdown(content) {
+    var render = getRender();
     return render.render(content);
 }
