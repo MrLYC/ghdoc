6ab0757b86002e5c8b4189833dfb003a67651f15
MrLYC <imyikong@gmail.com>
2018-03-01 16:39:06 +0000

    add toc anchor

--- src/js/markdown.js
+++ src/js/markdown.js
@@ -1,10 +1,14 @@
 import MarkdownIt from 'markdown-it';
 import mkHighlight from 'markdown-it-highlightjs';
+import mkToc from 'markdown-it-github-toc';
 
 export default function renderMarkdown(content) {
     var render = new MarkdownIt({
         html: true,
     });
     render.use(mkHighlight);
+    render.use(mkToc, {
+        anchorClassName: "toc-anchor",
+    });
     return render.render(content);
 }
