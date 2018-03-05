e20c6582ef2f0701c895c2f233c77efa5f4524eb
MrLYC <imyikong@gmail.com>
2018-03-05 12:55:14 +0000

    add custom highlight plugin

--- src/js/markdown.js
+++ src/js/markdown.js
@@ -1,14 +1,30 @@
 import MarkdownIt from 'markdown-it';
-import mkHighlight from 'markdown-it-highlightjs';
 import mkToc from 'markdown-it-github-toc';
 import mkFootnote from 'markdown-it-footnote';
 import mkTaskList from 'markdown-it-task-lists';
+import hljs from 'highlightjs';
+
+function highlightPlugin(render, opts) {
+    render.options.highlight = (code, lang) => {
+        if (hljs.getLanguage(lang)) {
+            return hljs.highlight(code, lang);
+        }
+        return hljs.highlightAuto(code, lang);
+    };
+
+    var defaultFence = render.renderer.rules.fence;
+    render.renderer.rules.fence = (...args) => {
+        return render.apply(this, args)
+            .replace('<code class="', '<code class="hljs ')
+            .replace('<code>', '<code class="hljs">');
+    };
+}
 
 function getRender() {
     var render = new MarkdownIt({
         html: true,
     });
-    render.use(mkHighlight);
+    render.use(highlightPlugin);
     render.use(mkFootnote);
     render.use(mkTaskList);
     render.use(mkToc, {
